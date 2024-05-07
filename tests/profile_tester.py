bdf6b73a982f4deca8a1f3d23eebde97cef0ae17
Sophie Probst <sophie.probst@students.unibe.ch>
2024-05-07 23:09:30 +0200

    work on profile testing

--- tests/profile_tester.py
+++ tests/profile_tester.py
@@ -1,17 +1,25 @@
 import unittest
 import pandas as pd
-from src.profile_class import Profile
-
+import sys
 import json
 import os
 
+sys.path.insert(0, 'src')
+from profile_class import Profile
+
 class TestProfile(unittest.TestCase):
+    """
+    Set up the test case by initializing a Profile object and loading tasklist data.
+    """
     def setUp(self):
         self.profile = Profile('Test')
         self.profile.tasklist = pd.read_csv('tasklist.csv')
         self.profile.tasklist['Deadline'] = pd.to_datetime(self.profile.tasklist['Deadline'])
 
 
+    """
+    Test the calculation of total tasks by summing completed, todo, and in-progress tasks.
+    """
     def test_total_tasks(self):
         self.profile.calculate_completed_tasks()
         self.profile.calculate_todo_tasks()
@@ -19,10 +27,18 @@ class TestProfile(unittest.TestCase):
         total_tasks = self.profile.completed_tasks + self.profile.todo_tasks + self.profile.inprogress_tasks
         self.assertEqual(len(self.profile.tasklist), total_tasks)
 
+
+    """
+    Test the calculation of total points by summing the points of all tasks.
+    """
     def test_calculate_total_points(self):
         self.profile.calculate_total_points()
         self.assertEqual(self.profile.total_points, self.profile.tasklist['Points'].sum())
 
+
+    """
+    Test the conversion of Profile object to a dictionary.
+    """
     def test_to_dict(self):
         profile_dict = self.profile.to_dict()
         self.assertEqual(profile_dict['name'], self.profile.name)
@@ -31,17 +47,46 @@ class TestProfile(unittest.TestCase):
         self.assertEqual(profile_dict['todo_tasks'], self.profile.todo_tasks)
         self.assertEqual(profile_dict['ongoing_tasks'], self.profile.inprogress_tasks)
 
+
+    """
+    Test the saving of Profile object to a JSON file and loading the data back.
+    """
     def test_save_to_json(self):
-        self.profile.save_to_json('test.json')
-        with open('test.json', 'r') as file:
+        self.profile.save_to_json('tests/test.json')
+        with open('tests/test.json', 'r') as file:
             data = json.load(file)
         self.assertEqual(data[0]['name'], self.profile.name)
-        os.remove('test.json')
+        self.assertEqual(data[0]['total_points'], self.profile.total_points)
+        self.assertEqual(data[0]['completed_tasks'], self.profile.completed_tasks)
+        self.assertEqual(data[0]['todo_tasks'], self.profile.todo_tasks)
+        self.assertEqual(data[0]['ongoing_tasks'], self.profile.inprogress_tasks)
+        os.remove('tests/test.json')
 
-    # def test_update_and_save(self)
-    #...
 
 
+    """
+    Update the Profile object's attributes and save it to a JSON file.
+    """
+    #Needs some more work...
+    def update_and_save(self):
+        self.profile.total_points = 100
+        self.profile.completed_tasks = 10
+        self.profile.todo_tasks = 5
+        self.profile.inprogress_tasks = 3
+        self.profile.save_to_json('tests/test.json')
+        with open('tests/test.json', 'r') as file:
+            data= json.load(file)
+            self.assertEqual(data[0]['name'], self.profile.name)
+            self.assertEqual(data[0]['total_points'], self.profile.total_points)
+            self.assertEqual(data[0]['completed_tasks'], self.profile.completed_tasks)
+            self.assertEqual(data[0]['todo_tasks'], self.profile.todo_tasks)
+            self.assertEqual(data[0]['ongoing_tasks'], self.profile.inprogress_tasks)
+            os.remove('tests/test.json')
+
+
+    """
+    Test the string representation of the Profile object.
+    """
     def test_str(self):
         profile_str = str(self.profile)
         self.assertIn(self.profile.name, profile_str)
@@ -51,10 +96,11 @@ class TestProfile(unittest.TestCase):
         self.assertIn(str(self.profile.inprogress_tasks), profile_str)
 
     # def test_calculate_missed_deadlines(self):
-    #     self.profile.tasklist = pd.read_csv('tasklist.csv')
-    #     self.profile.tasklist['Deadline'] = pd.to_datetime(self.profile.tasklist['Deadline'])
-    #     self.profile.calculate_missed_deadlines()
-    #     self.assertEqual(self.profile.missed_deadlines, 2)
+
+    def tear_down(self):
+        #check for the test.json file and remove it
+        if os.path.exists('test.json'):
+            os.remove('test.json')
 
 if __name__ == '__main__':
     unittest.main()
